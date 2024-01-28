4c75bcc0992eac033adc3e6855a588da8351c059
eborchard <eborchard@uiowa.edu>
2024-01-28 13:06:01 -0600

    Add category page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,8 +23,8 @@ const ngxUiLoaderConfig: NgxUiLoaderConfig = {
   text: "Loading...",
   textColor: "#FFFFFF",
   textPosition: "center-center",
-  bgsColor: "#7b1fa2",
-  fgsColor: "#7b1fa2",
+  bgsColor: "#0076CE",
+  fgsColor: "#0076CE",
   fgsType: SPINNER.doubleBounce,
   fgsSize: 100,
   hasProgressBar: false
